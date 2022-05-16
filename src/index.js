883eda3b162b1dd5265d1b3a4aa2e8291d012c44
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-15 23:46:01 -0300

    fix url to deployed server on heroku

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-	uri: 'http://localhost:5000',
+	uri: 'https://social-media-app-back-devtsp.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
