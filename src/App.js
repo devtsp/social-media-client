1d82e6df2807c160d01f0917edeec54451927dbf
Tomas <87679143+devtsp@users.noreply.github.com>
2022-06-25 19:53:42 -0300

    change to hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
